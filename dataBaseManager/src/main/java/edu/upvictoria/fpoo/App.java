23de99f8022e153753406b3547eec62a2e13bcab
ChristianAlberthRamosManzanares <2230321@upv.edu.mx>
2024-05-07 00:49:50 -0600

    1.- Ruta de trabajo/directorio donde PATH$ será la ruta relativa o obsoluta

--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/App.java
@@ -1,9 +1,14 @@
 package edu.upvictoria.fpoo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
